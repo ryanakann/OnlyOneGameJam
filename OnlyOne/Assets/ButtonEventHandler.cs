c54039793026c5394242ccc3000eed37489fadb2
ryanakann <ryanakann@gmail.com>
2019-08-02 23:27:24 -0400

    Demo Scene and Button Start

---
+++ OnlyOne/Assets/ButtonEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonEventHandler : MonoBehaviour {
+	public UnityEvent OnButtonDown;
+	public UnityEvent OnButtonStay;
+	public UnityEvent OnButtonUp;
+
+}
