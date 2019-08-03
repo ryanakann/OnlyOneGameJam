4f506b9d906afdf974e527c1404d0491c020c4d0
ryanakann <ryanakann@gmail.com>
2019-08-03 00:00:34 -0400

    Demo scene. Button and door

--- OnlyOne/Assets/ButtonEventHandler.cs
+++ OnlyOne/Assets/ButtonEventHandler.cs
@@ -8,4 +8,42 @@ public class ButtonEventHandler : MonoBehaviour {
 	public UnityEvent OnButtonStay;
 	public UnityEvent OnButtonUp;
 
+	public Transform buttonTop;
+	public Vector3 startPosition;
+	public Vector3 currentPosition;
+	public Vector3 difference;
+
+	public bool buttonPressed;
+	public bool buttonPressedLF;
+
+	private void Start () {
+		if (buttonTop) {
+			startPosition = buttonTop.position;
+		} else {
+			startPosition = Vector3.zero;
+		}
+
+		buttonPressed = false;
+		buttonPressedLF = buttonPressed;
+	}
+
+	private void Update () {
+		buttonPressed = (buttonTop.position.y - startPosition.y < -0.05f);
+
+		currentPosition = buttonTop.position;
+		difference = currentPosition - startPosition;
+
+		if (buttonPressed && !buttonPressedLF) {
+			print("Button Down!");
+			OnButtonDown.Invoke();
+		} else if (buttonPressedLF && !buttonPressed) {
+			print("Button Up!");
+			OnButtonUp.Invoke();
+		} else if (buttonPressed && buttonPressedLF) {
+			print("Button Stay!");
+			OnButtonStay.Invoke();
+		}
+
+		buttonPressedLF = buttonPressed;
+	}
 }
---
+++ OnlyOne/Assets/Door.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public enum OpenDirection { up, down, left, right }
+
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private Vector3 openVector;
+
+	public OpenDirection direction;
+	public float openDuration = 2f;
+
+	private void Start () {
+		startPosition = transform.position;
+
+		if (GetComponent<SpriteRenderer>()) {
+
+			switch (direction) {
+				case OpenDirection.up:
+					endPosition = startPosition + Vector3.up * GetComponent<SpriteRenderer>().size.y;
+					break;
+				case OpenDirection.down:
+					endPosition = startPosition + Vector3.down * GetComponent<SpriteRenderer>().size.y;
+					break;
+				case OpenDirection.left:
+					endPosition = startPosition + Vector3.left * GetComponent<SpriteRenderer>().size.x;
+					break;
+				case OpenDirection.right:
+					endPosition = startPosition + Vector3.right * GetComponent<SpriteRenderer>().size.x;
+					break;
+				default:
+					endPosition = startPosition;
+					break;
+			}
+		}
+
+		openVector = endPosition - startPosition;
+	}
+
+	public void Open () {
+		print("Opening door!");
+		StopAllCoroutines();
+		StartCoroutine(OpenCR());
+	}
+
+	private IEnumerator OpenCR () {
+		while ((endPosition - transform.position).sqrMagnitude > 0.01f) {
+			transform.position += openVector / openDuration * Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		transform.position = endPosition;
+	}
+
+	public void Close () {
+		print("Closing door!");
+		StopAllCoroutines();
+		StartCoroutine(CloseCR());
+	}
+
+	private IEnumerator CloseCR () {
+		while ((startPosition - transform.position).sqrMagnitude > 0.01f) {
+			print("Hey!");
+			transform.position -= openVector / openDuration * Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		transform.position = startPosition;
+	}
+}
