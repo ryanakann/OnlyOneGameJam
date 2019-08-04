fcd2bed5a3d6c6fac425ddb5a2c271c26764e73d
ryanakann <ryanakann@gmail.com>
2019-08-03 22:37:19 -0400

    Main theme

--- OnlyOne/Assets/Scripts/Intro.cs
+++ OnlyOne/Assets/Scripts/Intro.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour {
-	public float delay = 2f;
 	public float fadeInTime = 1f;
 	public float holdTime = 4f;
 	public float fadeOutTime = 1f;
 
+	public bool intro = false;
+	public bool awaitingKeyPress = false;
+
+	public UnityEngine.UI.Text awaitingText;
+
 	private void Start () {
+		awaitingText.color = new Color(awaitingText.color.r, awaitingText.color.g, awaitingText.color.b, 0f);
 		StartCoroutine(RunIntro());
 	}
 
+	private void Update () {
+		if (Input.anyKeyDown) {
+			if (intro) {
+				intro = false;
+				StopAllCoroutines();
+				StartCoroutine(AwaitKeyPress(0f));
+			} else if (awaitingKeyPress) {
+				awaitingKeyPress = false;
+				SceneController.LoadNextScene(true, fadeOutTime, 2f);
+			}
+		}
+	}
+
+	IEnumerator AwaitKeyPress (float fadeDuration) {
+		float t = 0f;
+		Color color = awaitingText.color;
+		if (fadeDuration > 0f) {
+			while (t < 1f) {
+				color.a = t;
+				t += Time.deltaTime / fadeDuration;
+				yield return new WaitForEndOfFrame();
+			}
+		}
+		color.a = 1f;
+		awaitingText.color = color;
+		awaitingKeyPress = true;
+	}
+
 	IEnumerator RunIntro () {
-		yield return new WaitForSeconds(delay);
 		FadeController.FadeIn(fadeInTime);
-		yield return new WaitForSeconds(fadeInTime + holdTime);
-		SceneController.LoadNextScene(true, fadeOutTime, 2f);
-		//FadeController.FadeOut(fadeOutTime);
-		//yield return new WaitForSeconds(fadeOutTime);
-		//int targetIndex = SceneManager.GetActiveScene().buildIndex + 1 % SceneManager.sceneCountInBuildSettings;
-		//print("Index: " + targetIndex);
-		//SceneManager.LoadScene(targetIndex);
+		yield return new WaitForSeconds(fadeInTime);
+		intro = true;
+		yield return new WaitForSeconds(holdTime);
+		intro = false;
+		StartCoroutine(AwaitKeyPress(1f));
+		yield return new WaitForSeconds(1f);
+		awaitingKeyPress = true;
 	}
 }
